17add22750738e98f3afe2a12b0d8b2465995c69
HARON <haronur@gmail.com>
2020-05-25 12:50:14 +0600

    04- Layout System Without

---
+++ src/layouts/Default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    
+    <nav-bar />
+    <hr>
+    <div class="container-fluid">
+      <div class="row content">
+
+        <side-bar />      
+        <router-view />
+
+      </div>
+
+      <footer-bar />
+    </div>
+  </div>
+</template>
+
+<script>
+  import NavBar from "../components/partial/NavBar";
+  import SideBar from "../components/partial/SideBar";
+  import FooterBar from "../components/partial/FooterBar";
+  export default {
+    components: {
+      NavBar,
+      SideBar,
+      FooterBar
+    }
+  }
+</script>
+
+<style>
+
+</style>
